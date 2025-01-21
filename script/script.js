f8d91d1aec3dab772000034661830fed5a439514
Hafsa Egal <hafsaegal915@gmail.com>
2025-01-21 10:41:24 +0300

    Assignment

---
+++ script/script.js
@@ -0,0 +1,16 @@
+dreamDestination = "Spain";
+console.log(dreamDestination);
+year = 2025;
+console.log(year);
+girl = true;
+boy = false;
+console.log(boy);
+const monthsOfTheYear = ["January", "Febuary", "March"];
+console.log(monthsOfTheYear);
+const favouriteCities = {
+  city1: "Mecca",
+  city2: "Jerusalam",
+  city3: "mombasa",
+  city4: "Amsterdam",
+};
+console.log(favouriteCities);
