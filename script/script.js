f70ad2aca08346d2e4f90b89777725797e33fb81
Hafsa Egal <hafsaegal915@gmail.com>
2025-01-22 10:37:12 +0300

    Weds assignment

--- script/script.js
+++ script/script.js
@@ -15,11 +15,38 @@ const favouriteCities = {
 };
 console.log(favouriteCities);
 
-const mombasa = {
-  favouritePlace: "Lamu Beach",
-  favouriteFoods: ["Biriyani", "chapati and maharagwe wa nazi"],
-  preferredStayDurationInWeeks: 3,
-  cold: false,
-  warm: true,
-};
-console.log(mombasa);
+// const mombasa = {
+//   favouritePlace: "Lamu Beach",
+//   favouriteFoods: ["Biriyani", "chapati and maharagwe wa nazi"],
+//   preferredStayDurationInWeeks: 3,
+//   cold: false,
+//   warm: true,
+// };
+// console.log(mombasa);
+firstName: "Hafsa";
+
+let age = 20;
+console.log(age);
+age = 21;
+
+const car = { name: "BMW", year: 2017, colour: "red", year: 2018 };
+console.log(car);
+const students = ["Ryan", 12];
+const num = 1;
+console.log(typeof num.toString());
+
+console.log("first".charAt(0));
+
+const newCar = [
+  "KIA",
+  2017,
+  (red = true),
+  (blue = false),
+  { previousOwner: "Sam" },
+];
+
+console.log("KIA".toLowerCase());
+console.log({ previousOwner: "sam".toUpperCase() });
+console.log("KIA".charAt(0));
+console.log(typeof newCar.toString());
+console.log(typeof newCar.toLocaleString());
